492937482eafcb879ac76764594f97f15d8dcd76
Jannis Teipel <teipel@gonicus.de>
2025-10-17 14:38:26 +0200

    Add loop

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,20 @@ fn read_request(len: u64) -> GreetRequest {
 }
 
 fn main() {
-    // Input
-    let size = read_size();
-    let request = read_request(size);
-
-    // Output
-    let response = GreetResponse {
-        result: request.x as i64 + request.y as i64,
-        greeting: format!("Hallo, {} {}", request.prename, request.surname),
-    };
-
-    let serialized = response.encode_to_vec();
-
-    stdout().write_all(&serialized.len().to_le_bytes()).expect("error writing size");
-    stdout().write_all(&serialized).expect("error writing string");
+    loop {    
+        // Input
+        let size = read_size();
+        let request = read_request(size);
+        
+        // Output
+        let response = GreetResponse {
+            result: request.x as i64 + request.y as i64,
+            greeting: format!("Hallo, {} {}", request.prename, request.surname),
+        };
+    
+        let serialized = response.encode_to_vec();
+    
+        stdout().write_all(&serialized.len().to_le_bytes()).expect("error writing size");
+        stdout().write_all(&serialized).expect("error writing string");
+    }
 }
