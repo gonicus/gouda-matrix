8d39e752743f956d8a2f7f269926efcdb1c5c8c0
Jannis Teipel <teipel@gonicus.de>
2026-01-14 08:29:19 +0000

    feat: add join rule to room change event

--- crates/mrhc_matrix_adapter/src/client.rs
+++ crates/mrhc_matrix_adapter/src/client.rs
@@ -867,10 +867,14 @@ impl ClientAbstraction for MatrixClient {
         let room = self.get_matrix_room(&request.room_id)?;
         let room = rooms::convert_to_proto(room).await?;
 
+        let join_rule = RoomJoinRule::try_from(room.join_rule)
+            .map_err(|_| errors::create_unknown("Invalid RoomJoinRule"))?;
+
         Ok(builder::RoomChangeEventBuilder::new(request.room_id)
             .change_user_id_list(room.user_id_list)
             .change_display_name(room.display_name)
             .change_unread_count(room.unread_count)
+            .change_join_rule(join_rule)
             .into_proto())
     }
 
