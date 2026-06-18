1a089a73261fcdafa53e8e8e64cdd9813f35614c
Jannis Teipel <teipel@gonicus.de>
2026-06-18 15:11:21 +0200

    Fix rustfmt errors

--- examples/rust_gouda_app/src/input.rs
+++ examples/rust_gouda_app/src/input.rs
@@ -162,12 +162,7 @@ impl InputWindow {
                     RemoveReaction,
                     self.config.remove_reaction.clone()
                 );
-                ui_action!(
-                    self,
-                    ui,
-                    GetMessage,
-                    self.config.get_message.clone()
-                );
+                ui_action!(self, ui, GetMessage, self.config.get_message.clone());
             });
     }
 }
