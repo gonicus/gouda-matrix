2ff04707c603568e063f635c9f5e12d4d47629fc
Jannis Teipel <teipel@gonicus.de>
2026-09-08 15:22:37 +0200

    Fix clippy errors

--- src/events.rs
+++ src/events.rs
@@ -895,10 +895,10 @@ impl EventExecutor {
     async fn exec_room_member_event(&mut self, room: Room, event: OriginalSyncRoomMemberEvent) {
         // Check if our user's membership has changed and if we need to handle this
         // change differently than a membership change for other users.
-        if Some(event.state_key.to_string()) == self.client.user_id().map(|f| f.to_string()) {
-            if self.process_own_membership_change(&room, &event).await {
-                return;
-            }
+        if Some(event.state_key.to_string()) == self.client.user_id().map(|f| f.to_string())
+            && self.process_own_membership_change(&room, &event).await
+        {
+            return;
         }
 
         if let MembershipChange::ProfileChanged {
@@ -972,7 +972,7 @@ impl EventExecutor {
                     .await;
             }
             MembershipChange::Joined => {
-                self.send_room_created_event_if_needed(&room).await;
+                self.send_room_created_event_if_needed(room).await;
             }
             _ => {
                 log::debug!("Treating it like any other membership change");
